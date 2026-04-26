448dc632ca53ddbddf11c9172bb7b7a84d8b1a0c
Giuseppe Benigno <giuseppe.benigno@gmail.com>
2026-04-26 11:04:32 +0200

    Initial commit: version 1.0 for KDE 5

---
+++ install.sh
@@ -0,0 +1,30 @@
+#! /bin/sh
+#
+# 	Part of kde-service-menu-reimage Version 1.0
+# 	Copyright (C) 2018-2018 Giuseppe Benigno <giuseppe.benigno(at)gmail.com>
+#
+# 	This program is free software: you can redistribute it and/or modify
+# 	it under the terms of the GNU General Public License as published by
+# 	the Free Software Foundation, either version 3 of the License, or
+# 	(at your option) any later version.
+#
+# 	This program is distributed in the hope that it will be useful,
+# 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+# 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# 	GNU General Public License for more details.
+#
+# 	You should have received a copy of the GNU General Public License
+# 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+bin_dir="$(kf5-config --path exe | sed "s/.*://")"
+sudo install -m 755 bin/* "${bin_dir}"
+
+desktop_dir="$(kf5-config --path services | sed "s/.*://")ServiceMenus/"
+sudo install -m 644 ServiceMenus/*.desktop "${desktop_dir}"
+
+doc_dir="$(kf5-config --prefix)/share/doc/kde-service-menu-steghide/"
+sudo install -d "${doc_dir}"
+sudo install -m 644 doc/* "${doc_dir}"
+
+echo "kde-service-menu-reimage has been installed. Enjoy!"
